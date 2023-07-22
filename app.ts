01cfd235a3ea2b1a5e44addc1b8412b29589e721
Mina Wanis <mwanis@turing.une.edu.au>
2023-07-23 02:44:29 +1000

    Implement placeStone function, checkForWin and render function

--- app.ts
+++ app.ts
@@ -1,56 +1,88 @@
 enum Player {
-    None = 0,
-    One = 1,
-    Two = 2
+    None,
+    Black,
+    White
 }
 
-export default class Gomoku {
+class Gomoku {
+    size: number;
     board: Player[][];
     currentPlayer: Player;
-    gameActive: boolean;
     boardElement: HTMLElement;
-    statusElement: HTMLElement;
+    gameOver: boolean;
+    messageElement: HTMLElement;
+    turnElement: HTMLElement;
 
-    constructor(public width: number = 15, public height: number = 15) {
-        this.boardElement = document.getElementById('board')!;
-        this.statusElement = document.getElementById('status')!;
-        this.reset();
-        document.getElementById('reset')!.addEventListener('click', () => this.reset());
+    constructor(size: number, boardElementId: string, messageElementId: string, turnElementId: string) {
+        this.size = size;
+        this.board = Array.from({ length: size }, () => Array(size).fill(Player.None));
+        this.currentPlayer = Player.Black;
+        this.boardElement = document.getElementById(boardElementId) as HTMLElement;
+        this.gameOver = false;
+        this.messageElement = document.getElementById(messageElementId) as HTMLElement;
+        this.turnElement = document.getElementById(turnElementId) as HTMLElement;
+    }
+
+    play(x: number, y: number) {
+        if (this.gameOver || this.board[y][x] !== Player.None) {
+            return;
+        }
+        this.placeStone(x, y, this.currentPlayer);
+    }
+
+    placeStone(x: number, y: number, player: Player) {
+        this.board[y][x] = player;
+        this.render();
+    
+        if (this.checkForWin(x, y, player)) {
+            this.gameOver = true;
+            this.messageElement.innerText = `${Player[player]} wins!`;
+            this.messageElement.style.display = "block";
+            this.turnElement.innerText = '';  // Clear the turn message
+        } else if (this.isBoardFull()) {
+            this.gameOver = true;
+            this.messageElement.innerText = 'It\'s a tie!';
+            this.messageElement.style.display = "block";
+            this.turnElement.innerText = '';  // Clear the turn message
+        } else {
+            this.currentPlayer = this.currentPlayer === Player.Black ? Player.White : Player.Black;
+            this.turnElement.innerText = `It's ${Player[this.currentPlayer]}'s turn`;
+        }
+    }
+
+    isBoardFull(): boolean {
+        return this.board.every(row => row.every(cell => cell !== Player.None));
+    }
+
+    checkForWin(x: number, y: number, player: Player): boolean {
+        const directions = [[1, 0], [0, 1], [1, 1], [1, -1]];
+        for (let [dx, dy] of directions) {
+            let count = 0;
+            for (let i = -4; i <= 4; i++) {
+                const nx = x + i * dx;
+                const ny = y + i * dy;
+                if (nx >= 0 && ny >= 0 && nx < this.size && ny < this.size && this.board[ny][nx] === player) {
+                    count++;
+                    if (count === 5) {
+                        return true;
+                    }
+                } else {
+                    count = 0;
+                }
+            }
+        }
+        return false;
     }
 
     reset() {
-        this.board = Array(this.height).fill(0).map(() => Array(this.width).fill(Player.None));
-        this.currentPlayer = Player.One;
-        this.gameActive = true;
+        this.board = Array.from({ length: this.size }, () => Array(this.size).fill(Player.None));
+        this.currentPlayer = Player.Black;
+        this.gameOver = false;
+        this.messageElement.style.display = "none";
+        this.turnElement.innerText = `It's ${Player[this.currentPlayer]}'s turn`;
         this.render();
     }
 
-    // render() {
-    //     this.boardElement.innerHTML = '';
-    //     this.board.forEach((row, y) => {
-    //         row.forEach((cell, x) => {
-    //             const cellElement = document.createElement('div');
-    //             cellElement.classList.add('cell');
-    //             cellElement.classList.add(cell === Player.One ? 'B' : cell === Player.Two ? 'W' : '');
-    //             cellElement.addEventListener('click', () => {
-    //                 if (cell === Player.None && this.gameActive && this.makeMove(x, y)) {
-    //                     const winner = this.checkWin();
-    //                     if (winner !== Player.None) {
-    //                         this.gameActive = false;
-    //                         this.statusElement.textContent = `Player ${winner === Player.One ? 'Black' : 'White'} wins!`;
-    //                     } else if (this.board.flat().every(cell => cell !== Player.None)) {
-    //                         this.gameActive = false;
-    //                         this.statusElement.textContent = `Game is a draw`;
-    //                     } else {
-    //                         this.statusElement.textContent = `Player ${this.currentPlayer === Player.One ? 'Black' : 'White'}'s turn`;
-    //                     }
-    //                 }
-    //             });
-    //             this.boardElement.appendChild(cellElement);
-    //         });
-    //         this.boardElement.appendChild(document.createElement('br'));
-    //     });
-    // }
     render() {
         this.boardElement.innerHTML = '';
         this.board.forEach((row, y) => {
@@ -59,21 +91,14 @@ export default class Gomoku {
             row.forEach((cell, x) => {
                 const cellElement = document.createElement('div');
                 cellElement.classList.add('cell');
-                const piece = document.createElement('span');
-                piece.classList.add(cell === Player.One ? 'black' : cell === Player.Two ? 'white' : 'empty');
-                cellElement.appendChild(piece);
+                if (cell !== Player.None) {
+                    const piece = document.createElement('span');
+                    piece.classList.add(cell === Player.Black ? 'black' : 'white');
+                    cellElement.appendChild(piece);
+                }
                 cellElement.addEventListener('click', () => {
-                    if (cell === Player.None && this.gameActive && this.makeMove(x, y)) {
-                        const winner = this.checkWin();
-                        if (winner !== Player.None) {
-                            this.gameActive = false;
-                            this.statusElement.textContent = `Player ${winner === Player.One ? 'Black' : 'White'} wins!`;
-                        } else if (this.board.flat().every(cell => cell !== Player.None)) {
-                            this.gameActive = false;
-                            this.statusElement.textContent = `Game is a draw`;
-                        } else {
-                            this.statusElement.textContent = `Player ${this.currentPlayer === Player.One ? 'Black' : 'White'}'s turn`;
-                        }
+                    if (!this.gameOver) {
+                        this.play(x, y);
                     }
                 });
                 rowElement.appendChild(cellElement);
@@ -81,22 +106,9 @@ export default class Gomoku {
             this.boardElement.appendChild(rowElement);
         });
     }
-
-
-
-    makeMove(x: number, y: number): boolean {
-        this.board[y][x] = this.currentPlayer;
-        this.currentPlayer = 3 - this.currentPlayer;
-        this.render();
-        return true;
-    }
-
-    checkWin(): Player {
-
-        return Player.None;
-    }
 }
 
-window.onload = () => {
-    const game = new Gomoku(5, 5);
-};
+const gomoku = new Gomoku(5, 'board', 'message', 'turn');
+const resetButton = document.getElementById('reset') as HTMLButtonElement;
+resetButton.addEventListener('click', () => gomoku.reset());
+gomoku.render();
